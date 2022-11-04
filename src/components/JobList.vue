8d841a98677c91d2684a73971c1a47ec26d77b68
Afandi Pratama Siregar <afandi@hungryhub.com>
2022-11-04 21:57:55 +0700

    first commit

---
+++ src/components/JobList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupee</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti,
+            dolor.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "@/types/job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      type: Array as PropType<Job[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
